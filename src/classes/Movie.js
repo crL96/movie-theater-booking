d0bcecbba63c2e3b0258c8b61d3e948fe04f04db
crL96 <190506813+crL96@users.noreply.github.com>
2026-02-02 16:15:21 +0100

    Add Movie class

---
+++ src/classes/Movie.js
@@ -0,0 +1,10 @@
+class Movie {
+    constructor(id, title, price, bookedSeats) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.bookedSeats = bookedSeats;
+    }
+}
+
+export default Movie;
