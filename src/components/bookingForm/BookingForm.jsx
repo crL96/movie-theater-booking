977c67b48b538f1292f4f3e8bf08bf662c96e497
crL96 <190506813+crL96@users.noreply.github.com>
2026-02-05 11:26:56 +0100

    Add function for validating user input

--- src/components/bookingForm/BookingForm.jsx
+++ src/components/bookingForm/BookingForm.jsx
@@ -6,9 +6,18 @@ function BookingForm({ setShowBookForm, seats, movie }) {
     e.preventDefault();
 
     const data = Object.fromEntries(new FormData(e.target));
+    data.phone = data.phone.trim();
+    data.name = data.name.trim();
     data.movieId = movie.id;
     data.seats = seats;
     data.cost = seats.length * movie.price;
+
+    const validationMsg = validateInput(data);
+    if (validationMsg != "") {
+      //TODO Call function to display msg to user
+      console.log(validationMsg);
+      return;
+    }
 
     try {
       const res = await fetch(API_URL + "/booking", {
@@ -30,16 +39,44 @@ function BookingForm({ setShowBookForm, seats, movie }) {
     }
   }
 
+  function validateInput(userInput) {
+    if (userInput.seats.length < 1) {
+      return "Please atleast one seat";
+    }
+    const namePattern = /^\p{L}{2,20} \p{L}{2,20}$/u;
+    if (!namePattern.test(userInput.name)) {
+      return "Invalid name format. Enter a first and last name, each 2-20 letters long, using only letters. Separate them with a single space.";
+    }
+    const phonePattern = /^(?=.{8,20}$)\+?[0-9]+(?:[ ][0-9]+|-[0-9]+)*$/;
+    if (!phonePattern.test(userInput.phone)) {
+      return "Invalid phone number format. 8-20 characters. May only include numbers, hyphens, spaces or optional + at beginning";
+    }
+    return "";
+  }
+
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
       <h2>Reserve your tickets</h2>
       <label htmlFor="name">
         Name
-        <input type="text" name="name" id="name" placeholder="John Smith" />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="John Smith"
+          maxLength={41}
+        />
       </label>
       <label htmlFor="phone">
         Phone
-        <input type="tel" name="phone" id="phone" placeholder="555-0212921" />
+        <input
+          type="tel"
+          name="phone"
+          id="phone"
+          placeholder="555-0212921"
+          minLength={8}
+          maxLength={20}
+        />
       </label>
       <button type="submit">Submit</button>
       <button type="button" onClick={() => setShowBookForm(false)}>
