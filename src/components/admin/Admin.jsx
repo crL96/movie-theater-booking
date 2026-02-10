28e4991e82f43b8a9c4f19c24d2ea6d28f4302e0
crL96 <190506813+crL96@users.noreply.github.com>
2026-02-10 15:14:13 +0100

    Add admin page comp with getAllMovies logic and placeholder MovieListItem comp

---
+++ src/components/admin/Admin.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { fetchAllMovies } from "../../services/api";
+import MovieListItem from "../movieListItem/MovieListItem";
+
+function Admin() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function getAllMovies() {
+      try {
+        setMovies(await fetchAllMovies());
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getAllMovies();
+  }, []);
+
+  return (
+    <main>
+      {movies.map((movie) => (
+        <MovieListItem movie={movie} key={movie.id} />
+      ))}
+    </main>
+  );
+}
+
+export default Admin;
---
+++ src/components/movieListItem/MovieListItem.jsx
@@ -0,0 +1,5 @@
+function MovieListItem({ movie }) {
+  return <p>{movie.title}</p>;
+}
+
+export default MovieListItem;
