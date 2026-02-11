5a6623bf16c60157a9df7bab7dfdf1250f18bb7d
crL96 <190506813+crL96@users.noreply.github.com>
2026-02-11 20:59:04 +0100

    Add class for bookings

---
+++ src/classes/Booking.js
@@ -0,0 +1,10 @@
+class Booking {
+    constructor(name, phone, movie, seats) {
+        this.name = name;
+        this.phone = phone;
+        this.movie = movie;
+        this.seats = seats;
+    }
+}
+
+export default Booking;
