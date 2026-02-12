bdebf18501ef10b4df3bfccf74217afdd15e6d88
crL96 <190506813+crL96@users.noreply.github.com>
2026-02-12 15:49:12 +0100

    Add url basename to router

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes.jsx";
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes, {
+  basename: "/movie-theater-booking/",
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
